279356880e322a26f1023228b2f5f0b0e9db14af
kianryan <kian@orangetentacle.co.uk>
2012-06-27 15:05:02 +0100

    SerializedAccount now generates line from fields.

--- DirectDebitAlbany/SerializedAccount.cs
+++ DirectDebitAlbany/SerializedAccount.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Reflection;
+using System.Text;
+
 namespace OrangeTentacle.DirectDebitAlbany
 {
     public interface ISerializedAccount
@@ -6,6 +10,7 @@ namespace OrangeTentacle.DirectDebitAlbany
         string SortCode { get; }
         string Name { get; }
         string Line();
+        string Line(string[] fields);
     }
 
     public class SerializedAccount : ISerializedAccount
@@ -13,13 +18,35 @@ namespace OrangeTentacle.DirectDebitAlbany
         internal SerializedAccount()
         {}
 
+        public readonly string[] DEFAULT_FIELDS = new [] { "SortCode", "Number", "Name" };
+
         public string Number { get; internal set; }
         public string SortCode { get; internal set; }
         public string Name { get; internal set; }
 
         public string Line()
         {
-            return SortCode + Number + Name; 
+            return Line(DEFAULT_FIELDS);
+        }
+
+        public string Line(string[] fields)
+        {
+            var composed = new StringBuilder();
+            
+            foreach(var field in fields)
+            {
+                if (field.ToUpper() == "LINE")
+                    throw new DirectDebitException("Parameters may not contain Line");
+
+                var p = typeof(SerializedAccount).GetProperty(field, 
+                        BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (p == null)
+                    throw new DirectDebitException("Property Not Found");
+
+                composed.Append(p.GetValue(this, null).ToString());
+            }
+
+            return composed.ToString();
         }
     }
 }
--- DirectDebitAlbanyTest/SerializedAccountTest.cs
+++ DirectDebitAlbanyTest/SerializedAccountTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Xunit.Extensions;
 
 namespace OrangeTentacle.DirectDebitAlbany.Test
 {
@@ -19,6 +20,66 @@ namespace OrangeTentacle.DirectDebitAlbany.Test
                     var composed = serialize.SortCode + serialize.Number + serialize.Name;
 
                     Assert.Equal(composed, line);
+                }
+
+                [Theory]
+                [InlineData("Number", "Name", "SortCode")]
+                [InlineData("Name", "SortCode", "Number")]
+                public void Takes_Properties(string prop1, string prop2, string prop3)
+                {
+                    var properties = new [] { prop1, prop2, prop3 };
+
+                    var account = new BankAccount(BankAccountTest.NUMBER, 
+                            BankAccountTest.SORTCODE, BankAccountTest.NAME);
+
+                    var serialize = account.Serialize();
+                    var line = serialize.Line(properties);
+
+                    var composed = "";
+                    foreach(var prop in properties)
+                    {
+                        var p = typeof(ISerializedAccount).GetProperty(prop);
+                        composed += p.GetValue(serialize, null).ToString();
+                    }
+
+                    Assert.Equal(composed, line);
+                }
+
+                [Fact]
+                public void Ignores_Case()
+                {
+                    var properties = new [] { "NUMBER" };
+                    var account = new BankAccount(BankAccountTest.NUMBER, 
+                            BankAccountTest.SORTCODE, BankAccountTest.NAME);
+                    var serialize = account.Serialize();
+
+                    var line = serialize.Line(properties);
+
+                    Assert.Equal(serialize.Number, line);
+                }
+
+                [Fact]
+                public void Invalid_Property_Throws_Exception()
+                {
+                    var properties = new [] { "Number", "Name", "SortCode", "Bob" };
+
+                    var account = new BankAccount(BankAccountTest.NUMBER, 
+                            BankAccountTest.SORTCODE, BankAccountTest.NAME);
+
+                    var serialize = account.Serialize();
+                    Assert.Throws<DirectDebitException>(() => serialize.Line(properties));
+                }
+
+                [Fact]
+                public void Cannot_Specify_Line()
+                {
+                    var properties = new [] { "Line" };
+
+                    var account = new BankAccount(BankAccountTest.NUMBER, 
+                            BankAccountTest.SORTCODE, BankAccountTest.NAME);
+
+                    var serialize = account.Serialize();
+                    Assert.Throws<DirectDebitException>(() => serialize.Line(properties));
                 }
             }
  
